27f5adad2fc0b513d9ee4ae449a56d57a7a74762
Jose Vinicios Batisata dos Santos <jose.vinicius.batista292@gmail.com>
2025-06-14 23:20:42 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,3 +1,16 @@
 function toggleProjeto(element) {
   element.classList.toggle('ativo');
 }
+
+// Ativar automaticamente baseado no sistema do usuário
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('dark-mode');
+}
+
+
+document.getElementById('whatsappBtn').addEventListener('click', () => {
+  const phoneNumber = '5511947391564'; // troca pelo número real, formato internacional (ex: 55 + DDD + número)
+  const message = "Olá, gostaria de saber mais sobre seus serviços.";
+const url = `https://wa.me/${phoneNumber}?text=${message}`;
+  window.open(url, '_blank');
+});
